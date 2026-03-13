91f3da7ca24f15e8fe4775e44046fa504e750f4d
irempazarli27 <irempazarli27@gmail.com>
2026-03-13 10:30:35 +0300

    fix port for render

--- server.js
+++ server.js
@@ -37,7 +37,9 @@ app.use(reviewRoutes);
 
 app.use(errorMiddleware);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server çalışıyor");
 });
 
